0c7dd279545b5e9bb0468c099bc2d3c22be78595
Rafael Chaves Freitas <rafael.chaves.freitas@gmail.com>
2016-03-02 13:09:54 -0300

    event list in progress (pagination not working)

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,34 +1,64 @@
 angular.module('mapasculturais.controllers', [])
 
-.controller('eventsCtrl', function($scope) {
+    .controller('eventsCtrl', ['$scope', 'mapas.service.event', function ($scope, eventApi) {
+            var api = eventApi(window.config.url);
 
-})
+            $scope.entities = [];
 
-.controller('spacesCtrl', function($scope) {
+            $scope.data = {
+                page: 1
+            };
 
-})
+            $scope.loadMore = function () {
+                console.log({
+                    '@limit': 10,
+                    '@page': $scope.data.page
+                });
+                var events = api.find({
+                    '@limit': 10,
+                    '@page': $scope.data.page
+                });
+                $scope.data.page++;
+                events.then(function (rs) {
+                    console.log(rs);
+                    rs.forEach(function (e) {
+                        $scope.entities.push(e);
+                    });
+                    $scope.$broadcast('scroll.infiniteScrollComplete');
+                });
 
-.controller('mapCtrl', function($scope) {
+                $scope.$on('$stateChangeSuccess', function () {
+                    $scope.loadMore();
+                });
+            };
 
-})
+        }])
 
-.controller('aboutCtrl', function($scope) {
+    .controller('spacesCtrl', function ($scope) {
 
-})
+    })
 
-.controller('starredCtrl', function($scope) {
+    .controller('mapCtrl', function ($scope) {
 
-})
+    })
 
-.controller('messagesCtrl', function($scope) {
+    .controller('aboutCtrl', function ($scope) {
 
-})
+    })
 
-.controller('navbarCtrl', function($scope, $location) {
-    $scope.location = $location.path();
+    .controller('starredCtrl', function ($scope) {
 
-})
+    })
 
-.controller('filterCtrl', function($scope) {
+    .controller('messagesCtrl', function ($scope) {
 
-})
+    })
+
+    .controller('navbarCtrl', function ($scope, $location) {
+        $scope.location = $location.path();
+
+    })
+
+    .controller('filterCtrl', function ($scope) {
+
+    })

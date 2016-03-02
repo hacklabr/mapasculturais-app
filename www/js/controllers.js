0d94d1077d8b1e8e75d6df76b75c03db282cd0cc
Rafael Chaves Freitas <rafael.chaves.freitas@gmail.com>
2016-03-02 17:53:18 -0300

    event list working without filters

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,28 +2,55 @@ angular.module('mapasculturais.controllers', [])
 
     .controller('eventsCtrl', ['$scope', 'mapas.service.event', function ($scope, eventApi) {
             var api = eventApi(window.config.url);
+            var _limit = 20;
+            var _page = 1;
+            var _endData = false;
+            var _lastGroup = null;
 
-            $scope.entities = [];
+            $scope.groups = [];
 
-            $scope.data = {
-                page: 1
+            $scope.filters = {
+                from: moment().format('Y-MM-DD'),
+                to: moment().add(7, 'days').format('Y-MM-DD')
             };
+            
+            $scope.$watch('filters', function(){
+                $scope.groups = [];
+                _page = 1;
+                _endData = false;
+            });
+            
+            $scope.moreDataCanBeLoaded = function(){
+                return !_endData;
+            }
 
             $scope.loadMore = function () {
-                console.log({
-                    '@limit': 10,
-                    '@page': $scope.data.page
+                var events = api.find($scope.filters.from, $scope.filters.to, {
+                    '@limit': _limit,
+                    '@page': _page
                 });
-                var events = api.find({
-                    '@limit': 10,
-                    '@page': $scope.data.page
-                });
-                $scope.data.page++;
+                
+                _page++;
+                
                 events.then(function (rs) {
-                    console.log(rs);
-                    rs.forEach(function (e) {
-                        $scope.entities.push(e);
+                    if(rs.length < _limit){
+                        _endData = true;
+                    }
+                    
+                    var _groups = api.group('YYYY-MM-DD HH:mm', rs);
+                    
+                    console.log(_groups);
+                    _groups.forEach(function (e) {
+                        if(_lastGroup && _lastGroup.date.format('YYYY-MM-DD HH:mm') === e.date.format('YYYY-MM-DD HH:mm')){
+                            e.events.forEach(function(event){
+                                _lastGroup.events.push(event);
+                            });
+                        }else{
+                            $scope.groups.push(e);
+                        }
+                            
                     });
+                    
                     $scope.$broadcast('scroll.infiniteScrollComplete');
                 });
 

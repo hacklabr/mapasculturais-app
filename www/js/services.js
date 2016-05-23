615f6a6887b261999159b699f8b917b7dfa0abfe
Bruno Martin <bruno.martin@hacklab.com.br>
2016-05-23 15:25:22 -0300

    fix typo in position

--- www/js/services.js
+++ www/js/services.js
@@ -22,7 +22,7 @@ angular.module('mapasculturais.services', [])
             name: 'SpCultura',
             url: 'http://spcultura.prefeitura.sp.gov.br/',
             map: {
-                latutude: -23.5408, 
+                latitude: -23.5408,
                 longitude: -46.6400,
                 zoom: 11
             }
@@ -32,7 +32,7 @@ angular.module('mapasculturais.services', [])
             name: 'SP Estado da Cultura',
             url: 'http://estadodacultura.sp.gov.br/',
             map: {
-                latutude: -22.61401087437029, 
+                latitude: -22.61401087437029,
                 longitude: -49.2132568359375,
                 zoom: 7
             }
@@ -42,7 +42,7 @@ angular.module('mapasculturais.services', [])
             name: 'Mapa Cultural do Ceará',
             url: 'http://mapa.cultura.ce.gov.br/',
             map: {
-                latutude: -5.063586123720585, 
+                latitude: -5.063586123720585,
                 longitude: -39.4134521484375,
                 zoom: 7
             }
@@ -52,7 +52,7 @@ angular.module('mapasculturais.services', [])
             name: 'Blumenau Mais Cultura',
             url: 'http://blumenaumaiscultura.com.br/',
             map: {
-                latutude: -26.883952223378103, 
+                latitude: -26.883952223378103,
                 longitude: -49.08708572387695,
                 zoom: 12
             }
@@ -62,7 +62,7 @@ angular.module('mapasculturais.services', [])
             name: 'Mapeamento Cultural de BH',
             url: 'http://mapeamentoculturalbh.pbh.gov.br/',
             map: {
-                latutude: -19.919614676628896, 
+                latitude: -19.919614676628896,
                 longitude: -43.97294998168945,
                 zoom: 12
             }
@@ -72,9 +72,19 @@ angular.module('mapasculturais.services', [])
             name: 'Mapas MT',
             url: 'https://mapas.cultura.mt.gov.br/',
             map: {
-                latutude: -13.549881446917126, 
+                latitude: -13.549881446917126,
                 longitude: -56.085205078125,
                 zoom: 6
+            }
+        },
+        mapasgovbr: {
+            prefix: 'mapasgovbr',
+            name: 'Mapas.cultura.gov.br',
+            url: 'http://mapas.cultura.gov.br/',
+            map: {
+                latitude: -15.792253570362446,
+                longitude: -47.900390625,
+                zoom: 5
             }
         },
         /* precisa atualizar o mapas para funcionar
@@ -83,16 +93,16 @@ angular.module('mapasculturais.services', [])
             name: 'Lugares da Cultura',
             url: 'http://lugaresdacultura.org.br/',
             map: {
-                latutude: -23.105628416305972, 
+                latitude: -23.105628416305972,
                 longitude: -45.84800720214843,
                 zoom: 10
             }
         }, */
     };
-    
+
     this.defineDataSource = function(prefix){
         $localStorage.config.dataSource = prefix;
-        
+
         this.dataSource = this.dataSources[prefix];
     }
 
@@ -100,7 +110,7 @@ angular.module('mapasculturais.services', [])
         this.dataSource = this.dataSources[$localStorage.config.dataSource];
     } else {
         $localStorage.config = {};
-        
+
         this.defineDataSource(this.dataSources[Object.keys(this.dataSources)[0]].prefix);
     }
 })
@@ -131,7 +141,7 @@ angular.module('mapasculturais.services', [])
     var getKey = function(event) {
         var key = moment(event.start.toString()).format('X') + '|' + event.occurrence_id;
         return config.dataSource.prefix + ':' + key;
-        
+
     }
 
     this.favorite = function(event) {
@@ -148,7 +158,7 @@ angular.module('mapasculturais.services', [])
     this.isFavorite = function(event) {
         return !!$localStorage.favoriteEvents[getKey(event)]
     }
-    
+
     this.clear = function(){
         $localStorage.favoriteEvents = {};
         sync();
